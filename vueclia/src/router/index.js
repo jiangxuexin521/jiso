1057877e07f637fa7dca9b174756c880a8079e3d
jiangxuexin521 <787063291@qq.com>
2020-10-24 14:36:35 +0800

    eeee

--- vueclia/src/router/index.js
+++ vueclia/src/router/index.js
@@ -3,8 +3,10 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test'
 import Test3 from '../views/Test3'
+import Test5 from '../views/Test5'
 import A from '@/views/A'
 import B from '@/views/B'
+import IVueX from '@/views/Vuex'
 
 Vue.use(VueRouter)
 
@@ -29,10 +31,21 @@ const routes = [
     path:'/test3/:id',
     name:'test3',
     component:Test3
-  },{
+  },
+  {
+    path:'/test5',
+    component:Test5,
+    alias:'/aa'
+  },
+  {
+    path:'/vuex',
+    component:IVueX
+  },
+  {
     path:'/:id',
-    redirect:'/test3/:id'
-  }
+    redirect:'/test3/:id',
+  },
+  
   
 ]
 
